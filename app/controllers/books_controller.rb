fc57f185085bba6b0b7aaa4b0abf605d84042da0
john-otienoh <johnteclaire@gmail.com>
2025-03-01 20:33:00 +0300

    CRUD functionalities implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,49 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[ show edit update destroy ]
+
   def index
+    @books = Book.all
   end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: "Book added successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: "Book update successful."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: "Book deleted successfully."
+  end
+
+  private
+    def set_book
+      puts "set_book called with params[:id] = #{params[:id]}"
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
